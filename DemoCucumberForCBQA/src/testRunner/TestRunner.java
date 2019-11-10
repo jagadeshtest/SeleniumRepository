9a58233dd38a73296d65a68a3f0ac265d58618a6
e3004592 <e3004592@MAA2-LTD47MX52.FNFIS.com>
2019-11-10 10:50:15 +0530

    Jags Commit

--- DemoCucumberForCBQA/src/testRunner/TestRunner.java
+++ DemoCucumberForCBQA/src/testRunner/TestRunner.java
@@ -1,15 +1,17 @@
 package testRunner;
 
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class TestRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-JUnitCore.runClasses(Runner.class);
-/*for(Failure failure:result.getFailures()){
-	System.out.println(failure.toString());
-}
-System.out.println(result.wasSuccessful());*/
+		Result result=JUnitCore.runClasses(Runner.class);
+		for(Failure failure:result.getFailures()){
+			System.out.println(failure.toString());
+		}
+		System.out.println(result.wasSuccessful());
 	}
 }
