67be591f243a000a222ad43ff57749e9d524703b
e3004592 <e3004592@MAA2-LTD47MX52.FNFIS.com>
2019-11-10 08:23:11 +0530

    Hi

---
+++ DemoCucumberForCBQA/src/testRunner/TestRunner2.java
@@ -0,0 +1,15 @@
+package testRunner;
+
+import org.junit.runner.JUnitCore;
+
+public class TestRunner2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JUnitCore.runClasses(Runner.class);
+/*for(Failure failure:result.getFailures()){
+	System.out.println(failure.toString());
+}
+System.out.println(result.wasSuccessful());*/
+	}
+}
